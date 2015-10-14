5faba2bc1786117ebbdb5d8b85408971f2de10f1
nenghu <243188056@qq.com>
2015-10-14 15:17:55 +0800

    测试提交代码

--- BaiduDemo/BaiduDemo/ViewController.m
+++ BaiduDemo/BaiduDemo/ViewController.m
@@ -16,6 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIView * view = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    view.backgroundColor = [UIColor redColor];
+        [self.view addSubview:view];
+    [UIView animateWithDuration:2.0 animations:^{
+        view.transform = CGAffineTransformMakeScale(2.0, 2.0);
+    } completion:^(BOOL finished) {
+        if (finished) {
+            view.transform = CGAffineTransformMakeScale(1.0, 1.0);
+        }
+    }];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
